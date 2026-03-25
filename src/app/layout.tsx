786248dd4f3decbf7f7622f8cd3b27f448882044
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2026-03-25 15:03:07 +0000

    🔨 Fix - Language of website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", inter.variable)}>
+    <html lang="is" className={cn("font-sans", inter.variable)}>
       <body className={cn(plusJakartaSans.className, "antialiased")}>
         {children}
         <Toaster />
